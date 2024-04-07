dcbd54117c4a8eb570376494439ce90df2b26537
Elias Eggenberger <elias.eggenberger@hm.edu>
2024-04-07 11:55:48 +0200

    added an image picker

--- PlantCare/pages/newplant.tsx
+++ PlantCare/pages/newplant.tsx
@@ -1,21 +1,30 @@
-import { View, Text, StyleSheet, Image } from "react-native";
-import React from 'react'; // Don't forget to import React
-import BackgroundImageBox from "../components/backgroundimage";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, Button } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default function NewPlants() {
+    const [image, setImage] = useState<string | null>(null);
+
+    const selectImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images
+        });
+        if (!result.canceled) {
+            setImage(result.assets[0].uri)
 
 
+        }
+        
 
 
-
-
-
-export default function NewPlants()
-{
+        
+    }
 
     return (
-        <View> 
-            </View> 
-
-    )
-
-
+        <View>
+            <Text> Hallo </Text>
+            <Button title="Select a plant picture" onPress={selectImage} />
+            {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        </View>
+    );
 }
